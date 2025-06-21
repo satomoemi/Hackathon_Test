b11d217afb63b9f07c742cd95f2f148369a55f09
satomoemi <meiyin6888@gmail.com>
2025-06-21 21:02:03 +0900

    ハッカソン入門テスト

---
+++ example.py
@@ -0,0 +1,24 @@
+print("hello world")
+
+def greet():
+  g = "こんにちは"
+  return g
+
+greet()
+
+def print_name(name):
+  return "私の名前は{}です".format(name)
+
+print_name("Moemi")
+
+def get_greet():
+  return "おはようございます"
+
+print(get_greet())
+
+def add(a,b):
+  return a + b
+
+print(add(1,1))
+
+
